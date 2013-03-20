b312077e6bd21ae4df464af421d9a1f4fa0c7b31
Kenneth Moreland <kmorel@sandia.gov>
2013-03-20 06:39:15 -0600

    Initial souces copied from threshold scaling example.

---
+++ MCScalingMPI.cxx
@@ -0,0 +1,407 @@
+#include <ThresholdScalingConfig.h>
+
+#include <mpi.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <fstream>
+#include <iostream>
+
+#include <vector>
+
+#define DAX_DEVICE_ADAPTER DAX_DEVICE_ADAPTER_ERROR
+
+#include <dax/CellTag.h>
+#include <dax/CellTraits.h>
+#include <dax/cont/ArrayContainerControlBasic.h>
+#include <dax/cont/ArrayHandle.h>
+#include <dax/cont/DeviceAdapter.h>
+#include <dax/cont/GenerateTopology.h>
+#include <dax/cont/Scheduler.h>
+#include <dax/cont/Timer.h>
+#include <dax/cont/UniformGrid.h>
+#include <dax/cont/UnstructuredGrid.h>
+
+#include <dax/worklet/Magnitude.h>
+#include <dax/worklet/Threshold.h>
+
+#include <dax/cont/DeviceAdapterSerial.h>
+
+#define LOAD_DATA 1
+//#define REMOVE_UNUSED_POINTS 1
+
+class ThresholdExampleMPIError : public dax::cont::ErrorControl
+{
+public:
+  ThresholdExampleMPIError(const char *file,
+                           dax::Id line,
+                           const char *funcall,
+                           int errorCode)
+  {
+    std::stringstream message;
+    message << file << ":" << line
+            << ": Failed MPI call (" << funcall << ")\n";
+    char errormsg[MPI_MAX_ERROR_STRING];
+    int dummy;
+    MPI_Error_string(errorCode, errormsg, &dummy);
+    message << errormsg;
+    this->SetMessage(message.str());
+  }
+};
+
+#define MPICall(funcall) \
+  { \
+  int __my_result = funcall; \
+  if (__my_result != MPI_SUCCESS) \
+    { \
+    throw ThresholdExampleMPIError(__FILE__, __LINE__, #funcall, __my_result); \
+    } \
+  }
+
+class ThresholdExample
+{
+private:
+
+  typedef dax::cont::ArrayContainerControlTagBasic Container;
+  typedef dax::cont::DeviceAdapterTagSerial DeviceAdapter;
+
+  typedef dax::cont::UniformGrid<DeviceAdapter> UniformGridType;
+  typedef dax::cont::UnstructuredGrid<
+      dax::CellTagHexahedron,Container,Container,DeviceAdapter>
+      UnstructuredGridType;
+
+  typedef dax::cont::ArrayHandle<
+      dax::Scalar, dax::cont::ArrayContainerControlTagBasic, DeviceAdapter>
+      ArrayHandleScalar;
+  typedef dax::cont::ArrayHandle<
+      dax::Vector3, dax::cont::ArrayContainerControlTagBasic, DeviceAdapter>
+      ArrayHandleVector;
+
+  int Rank;
+
+  FILE *LogFile;
+
+  void LogTime(MPI_Comm comm,
+               std::string description,
+               int trial,
+               float value)
+  {
+    int numProcesses;
+    MPI_Comm_size(comm, &numProcesses);
+
+    float average;
+    MPI_Reduce(&value, &average, 1, MPI_FLOAT, MPI_SUM, 0, comm);
+    average /= numProcesses;
+
+    float minimum;
+    MPI_Reduce(&value, &minimum, 1, MPI_FLOAT, MPI_MIN, 0, comm);
+
+    float maximum;
+    MPI_Reduce(&value, &maximum, 1, MPI_FLOAT, MPI_MAX, 0, comm);
+
+    if (this->Rank == 0)
+      {
+      std::cout << description << ": " << minimum
+                << " - " << average << " - " << maximum << std::endl;
+      fprintf(this->LogFile, "%s,%d,%d,%g,%g,%g\n",
+              description.c_str(), numProcesses, trial,
+              maximum, average, minimum);
+      }
+  }
+
+  static UniformGridType CreateUniformGrid(int piece, int numPieces)
+  {
+    dax::Extent3 cellExtent;
+    cellExtent.Min = dax::make_Id3(0, 0, 0);
+    cellExtent.Max = dax::make_Id3(GRID_SIZE-2, GRID_SIZE-2, GRID_SIZE-2);
+
+    int lowPiece = 0;
+    int highPiece = numPieces;
+    int axis = 0;
+    while ((highPiece - lowPiece) > 1)
+      {
+      int midPiece = (highPiece + lowPiece)/2;
+      dax::Id midCell = (cellExtent.Max[axis] + cellExtent.Min[axis] + 1)/2;
+      if (piece < midPiece)
+        {
+        cellExtent.Max[axis] = midCell-1;
+        highPiece = midPiece;
+        }
+      else
+        {
+        cellExtent.Min[axis] = midCell;
+        lowPiece = midPiece;
+        }
+      axis++;
+      if (axis > 2) { axis = 0; }
+      }
+    assert(lowPiece == piece);
+    assert(highPiece == piece+1);
+
+    dax::Extent3 pointExtent;
+    pointExtent.Min = cellExtent.Min;
+    pointExtent.Max = cellExtent.Max + dax::make_Id3(1, 1, 1);
+
+    UniformGridType grid;
+    grid.SetExtent(pointExtent);
+    return grid;
+  }
+
+  static void ReadSupernovaData(const UniformGridType &grid,
+                                MPI_Comm comm,
+                                std::vector<dax::Scalar> &buffer)
+  {
+    assert(sizeof(float) == sizeof(dax::Scalar));
+
+    MPI_File fd;
+    MPICall(MPI_File_open(comm,
+                          const_cast<char*>(DATA_FILE),
+                          MPI_MODE_RDONLY,
+                          MPI_INFO_NULL,
+                          &fd));
+
+    dax::Extent3 extent = grid.GetExtent();
+    int arrayOfSizes[3];
+    int arrayOfSubSizes[3];
+    int arrayOfStarts[3];
+    for (int i = 0; i < 3; i++)
+      {
+      arrayOfSizes[i] = GRID_SIZE;
+      arrayOfSubSizes[i] = extent.Max[i] - extent.Min[i] + 1;
+      arrayOfStarts[i] = extent.Min[i];
+      }
+
+    // Create a view in MPIIO
+    MPI_Datatype view;
+    MPICall(MPI_Type_create_subarray(3,
+                                     arrayOfSizes,
+                                     arrayOfSubSizes,
+                                     arrayOfStarts,
+                                     MPI_ORDER_FORTRAN,
+                                     MPI_FLOAT,
+                                     &view));
+    MPICall(MPI_Type_commit(&view));
+    MPICall(MPI_File_set_view(fd,
+                              0,
+                              MPI_FLOAT,
+                              view,
+                              const_cast<char *>("native"),
+                              MPI_INFO_NULL));
+    MPICall(MPI_Type_free(&view));
+
+    // Figure out how many floats to read.
+    int length = arrayOfSubSizes[0]*arrayOfSubSizes[1]*arrayOfSubSizes[2];
+
+    buffer.resize(length);
+    MPICall(MPI_File_read_all(fd,
+                              &buffer.at(0),
+                              length,
+                              MPI_FLOAT,
+                              MPI_STATUS_IGNORE));
+
+    MPICall(MPI_File_close(&fd));
+  }
+
+  static void CreateMagnitudeField(const UniformGridType &grid,
+                                   ArrayHandleScalar &data)
+  {
+    dax::cont::Scheduler<DeviceAdapter> scheduler;
+    scheduler.Invoke(dax::worklet::Magnitude(),
+                     grid.GetPointCoordinates(),
+                     data);
+  }
+
+  void RunThreshold(const UniformGridType &grid,
+                    const ArrayHandleScalar &inArray,
+                    MPI_Comm comm,
+                    int trial)
+  {
+    MPI_Barrier(comm);
+    dax::cont::Timer<DeviceAdapter> timer;
+    typedef dax::cont::ArrayHandle<
+        dax::Id, DAX_DEFAULT_ARRAY_CONTAINER_CONTROL_TAG, DeviceAdapter>
+        ClassifyResultType;
+    typedef dax::cont::GenerateTopology<
+        dax::worklet::ThresholdTopology,ClassifyResultType>
+        GenerateTopologyType;
+
+    dax::cont::Scheduler<DeviceAdapter> scheduler;
+
+    // Run classify algorithm (determine how many cells are passed).
+#ifdef LOAD_DATA
+        const dax::Scalar LOW_SCALAR = 0.07;
+        const dax::Scalar HIGH_SCALAR = 1.0;
+#else // LOAD_DATA
+        const dax::Scalar LOW_SCALAR = 50.0;
+        const dax::Scalar HIGH_SCALAR = 200.0;
+#endif // LOAD_DATA
+    ClassifyResultType classificationArray;
+    dax::cont::Timer<DeviceAdapter> classifyTimer;
+    scheduler.Invoke(dax::worklet::ThresholdClassify<dax::Scalar>(
+                       LOW_SCALAR, HIGH_SCALAR),
+                     grid,
+                     inArray,
+                     classificationArray);
+    dax::Scalar elapsedClassify = classifyTimer.GetElapsedTime();
+
+    // Build thresholded topology.
+    GenerateTopologyType resolveTopology(classificationArray);
+#ifndef REMOVE_UNUSED_POINTS
+    resolveTopology.SetRemoveDuplicatePoints(false);
+#endif
+    UnstructuredGridType outGrid;
+    scheduler.Invoke(resolveTopology, grid, outGrid);
+
+#ifdef REMOVE_UNUSED_POINTS
+    // Compact scalar array to new topology.
+    ArrayHandleScalar outArray;
+    resolveTopology.CompactPointField(inArray, outArray);
+
+    // Copy grid information to host, if necessary.
+    outGrid.GetCellConnections().GetPortalConstControl();
+    outGrid.GetPointCoordinates().GetPortalConstControl();
+    outArray.GetPortalConstControl();
+#endif
+
+    dax::Scalar time = timer.GetElapsedTime();
+
+    int localNumCells = outGrid.GetNumberOfCells();
+    int globalNumCells;
+    MPI_Reduce(&localNumCells, &globalNumCells, 1, MPI_INT, MPI_SUM, 0, comm);
+    if (this->Rank == 0)
+      {
+      std::cout << "Number of output cells: " << globalNumCells << std::endl;
+      }
+
+    this->LogTime(comm, "Dax-MPI", trial, time);
+    this->LogTime(comm, "classify", trial, elapsedClassify);
+    this->LogTime(comm, "scan", trial, dax::cont::scheduling::ScanTime);
+    this->LogTime(comm, "fill-index", trial, dax::cont::scheduling::FillIndexTime);
+    this->LogTime(comm, "upper-bounds", trial, dax::cont::scheduling::UpperBoundsTime);
+    this->LogTime(comm, "worklet", trial, dax::cont::scheduling::WorkletTime);
+    // this->LogTime(cont, "remove-duplicate-points", trial, dax::cont::scheduling::RemoveDuplicatePointsTime);
+  }
+
+  void TryWithComm(MPI_Comm comm)
+  {
+    int numProcesses;
+    MPI_Comm_size(comm, &numProcesses);
+
+    UniformGridType grid = this->CreateUniformGrid(this->Rank, numProcesses);
+
+#ifdef LOAD_DATA
+    if (this->Rank == 0) { std::cout << "Reading data..." << std::endl; }
+    std::vector<dax::Scalar> buffer;
+    ReadSupernovaData(grid, comm, buffer);
+    if (this->Rank == 0) { std::cout << "Data read." << std::endl; }
+#endif // LOAD_DATA
+
+    for (int trial = 0; trial < NUM_TRIALS; trial++)
+      {
+#ifdef LOAD_DATA
+      ArrayHandleScalar inArray =
+          dax::cont::make_ArrayHandle(buffer, Container(), DeviceAdapter());
+#else // LOAD_DATA
+      if (this->Rank == 0) { std::cout << "Creating data" << std::endl; }
+      ArrayHandleScalar inArray;
+      CreateMagnitudeField(grid, inArray);
+#endif // LOAD_DATA
+      assert(grid.GetNumberOfPoints() == inArray.GetNumberOfValues());
+
+      if (this->Rank == 0)
+        {
+        std::cout << "Computing Threshold, " << numProcesses << " processes..."
+                  << std::endl;
+        }
+      this->RunThreshold(grid, inArray, comm, trial);
+      }
+  }
+
+  void TryEachCommSize()
+  {
+    int numProcesses;
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+    if (numProcesses == 1)
+      {
+      std::cout << "*** WARNING: Only one process in parallel job. ***"
+                << std::endl;
+      std::cout << "Did you run this program with mpirun or mpiexec?"
+                << std::endl;
+      }
+
+    for (int groupSize = 1; groupSize <= numProcesses; groupSize++)
+      {
+      bool inGroup = this->Rank < groupSize;
+      MPI_Comm comm;
+      MPI_Comm_split(MPI_COMM_WORLD,
+                     static_cast<int>(inGroup),
+                     this->Rank,
+                     &comm);
+
+      if (inGroup)
+        {
+        this->TryWithComm(comm);
+        }
+
+      MPI_Comm_free(&comm);
+      MPI_Barrier(MPI_COMM_WORLD);
+      }
+  }
+
+public:
+  ThresholdExample(int argc, char *argv[])
+  {
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &this->Rank);
+
+    this->LogFile = NULL;
+    if (this->Rank == 0)
+      {
+      this->LogFile = fopen("ThresholdScalingMPI.csv", "w");
+      assert(this->LogFile != NULL);
+      fprintf(this->LogFile, "Implementation,Threads,Trial,Seconds\n");
+      }
+  }
+  ~ThresholdExample()
+  {
+    if (this->Rank == 0)
+      {
+      fclose(this->LogFile);
+      this->LogFile = NULL;
+      }
+    MPI_Finalize();
+  }
+
+  int Run()
+  {
+    try
+      {
+      this->TryEachCommSize();
+      }
+    catch (dax::cont::Error error)
+      {
+      std::cout << "Caught Dax error: " << std::endl
+                << error.GetMessage() << std::endl;
+      return 1;
+      }
+    catch (...)
+      {
+      std::cout << "Undefined error caught." << std::endl;
+      return 1;
+      }
+
+    return 0;
+  }
+
+private:
+  ThresholdExample(const ThresholdExample &); // Not implemented.
+  void operator=(const ThresholdExample &); // Not implemented.
+};
+
+int main(int argc, char *argv[])
+{
+  return ThresholdExample(argc, argv).Run();
+}
+
---
+++ MCScalingTbb.cxx
@@ -0,0 +1,310 @@
+#include <ThresholdScalingConfig.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <fstream>
+#include <iostream>
+
+#include <vector>
+
+#define DAX_DEVICE_ADAPTER DAX_DEVICE_ADAPTER_ERROR
+
+#include <dax/CellTag.h>
+#include <dax/CellTraits.h>
+#include <dax/cont/ArrayContainerControlBasic.h>
+#include <dax/cont/ArrayHandle.h>
+#include <dax/cont/DeviceAdapter.h>
+#include <dax/cont/GenerateTopology.h>
+#include <dax/cont/Scheduler.h>
+#include <dax/cont/Timer.h>
+#include <dax/cont/UniformGrid.h>
+#include <dax/cont/UnstructuredGrid.h>
+
+#include <dax/worklet/Magnitude.h>
+#include <dax/worklet/Threshold.h>
+
+#include <dax/tbb/cont/DeviceAdapterTBB.h>
+
+#include <tbb/task_scheduler_init.h>
+
+#define LOAD_DATA 1
+//#define REMOVE_UNUSED_POINTS 1
+
+class CopyWorklet : public dax::exec::WorkletMapField
+{
+public:
+  typedef void ControlSignature(Field(In), Field(Out));
+  typedef _2 ExecutionSignature(_1);
+
+  template<typename T>
+  DAX_EXEC_EXPORT T operator()(const T &inValue) const
+  {
+    return inValue;
+  }
+};
+
+class ThresholdExample
+{
+private:
+
+  typedef dax::cont::ArrayContainerControlTagBasic Container;
+  typedef dax::tbb::cont::DeviceAdapterTagTBB DeviceAdapter;
+
+  typedef dax::cont::UniformGrid<DeviceAdapter> UniformGridType;
+  typedef dax::cont::UnstructuredGrid<
+      dax::CellTagHexahedron,Container,Container,DeviceAdapter>
+      UnstructuredGridType;
+
+  typedef dax::cont::ArrayHandle<
+      dax::Scalar, dax::cont::ArrayContainerControlTagBasic, DeviceAdapter>
+      ArrayHandleScalar;
+  typedef dax::cont::ArrayHandle<
+      dax::Vector3, dax::cont::ArrayContainerControlTagBasic, DeviceAdapter>
+      ArrayHandleVector;
+
+  static UniformGridType CreateUniformGrid()
+  {
+    UniformGridType grid;
+    grid.SetExtent(dax::make_Id3(0, 0, 0),
+                   dax::make_Id3(GRID_SIZE-1, GRID_SIZE-1, GRID_SIZE-1));
+    return grid;
+  }
+
+  static void ReadSupernovaData(std::vector<dax::Scalar> &buffer)
+  {
+    assert(sizeof(float) == sizeof(dax::Scalar));
+
+    FILE *fd = fopen(DATA_FILE, "rb");
+    assert(fd != NULL);
+
+    buffer.resize(GRID_SIZE*GRID_SIZE*GRID_SIZE);
+    fread(&buffer.front(), sizeof(float), GRID_SIZE*GRID_SIZE*GRID_SIZE, fd);
+    assert(ferror(fd) == 0);
+
+    fclose(fd);
+  }
+
+  static void CreateMagnitudeField(const UniformGridType &grid,
+                                   ArrayHandleScalar &data)
+  {
+    dax::cont::Scheduler<DeviceAdapter> scheduler;
+    scheduler.Invoke(dax::worklet::Magnitude(),
+                     grid.GetPointCoordinates(),
+                     data);
+  }
+
+  static void WriteGradientMagnitude(const ArrayHandleScalar &data)
+  {
+    FILE *fd = fopen("/Users/kmorel/Downloads/gradients.dat", "wb");
+    assert(fd != NULL);
+
+    typedef ArrayHandleScalar::PortalConstControl PortalType;
+    PortalType dataPortal = data.GetPortalConstControl();
+
+    for (PortalType::IteratorType iter = dataPortal.GetIteratorBegin();
+         iter != dataPortal.GetIteratorEnd();
+         iter++)
+      {
+      dax::Scalar value = *iter;
+      fwrite(&value, sizeof(dax::Scalar), 1, fd);
+      }
+
+    fclose(fd);
+  }
+
+  template<typename Stream>
+  static void PrintContentsToStream(UnstructuredGridType& grid, Stream &stream)
+  {
+    const dax::Id num_points(grid.GetNumberOfPoints());
+    const dax::Id num_cells(grid.GetNumberOfCells());
+
+    //dump header
+    stream << "# vtk DataFile Version 3.0"  << std::endl;
+    stream << "vtk output" << std::endl;
+    stream << "ASCII"  << std::endl;
+    stream << "DATASET UNSTRUCTURED_GRID" << std::endl;
+    stream << "POINTS " << num_points << " float" << std::endl;
+
+    std::vector<dax::Vector3> contPoints(num_points);
+    grid.GetPointCoordinates().CopyInto(contPoints.begin());
+
+    for(dax::Id i=0; i < num_points; ++i)
+      {
+      dax::Vector3 coord = contPoints[i];
+      stream << coord[0] << " " << coord[1] << " " << coord[2] << " ";
+      if(i%4==3)
+        {
+        stream << std::endl; //pump new line after each 4th vector
+        }
+      }
+    if(num_points%4==3)
+      {
+      stream << std::endl;
+      }
+
+    const int NUM_VERTICES =
+        dax::CellTraits<dax::CellTagHexahedron>::NUM_VERTICES;
+    //print cells
+    stream << "CELLS " << num_cells << " " << num_cells * (NUM_VERTICES+1)
+           << std::endl;
+
+    std::vector<dax::Id> contTopo(num_cells*NUM_VERTICES);
+    grid.GetCellConnections().CopyInto(contTopo.begin());
+
+    dax::Id index=0;
+    for(dax::Id i=0; i < num_cells; ++i,index+=8)
+      {
+      stream << NUM_VERTICES << " ";
+      stream << contTopo[index+0] << " ";
+      stream << contTopo[index+1] << " ";
+      stream << contTopo[index+2] << " ";
+      stream << contTopo[index+3] << " ";
+      stream << contTopo[index+4] << " ";
+      stream << contTopo[index+5] << " ";
+      stream << contTopo[index+6] << " ";
+      stream << contTopo[index+7] << " ";
+
+      stream << std::endl;
+      }
+    stream << std::endl;
+    stream << "CELL_TYPES " << num_cells << std::endl;
+    for(dax::Id i=0; i < num_cells; ++i)
+      {
+      stream << "12" << std::endl; //11 is voxel && 12 is hexa
+      }
+  }
+
+public:
+  static int Run()
+  {
+    FILE *fd = fopen("ThresholdScalingDax.csv", "w");
+    assert(fd != NULL);
+    fprintf(fd, "Implementation,Threads,Trial,Seconds\n");
+
+    try
+    {
+#ifdef LOAD_DATA
+    std::cout << "Reading data..." << std::endl;
+    std::vector<dax::Scalar> buffer;
+    ReadSupernovaData(buffer);
+    std::cout << "Data read." << std::endl;
+#endif
+
+    UniformGridType grid = CreateUniformGrid();
+
+    int maxNumThreads = tbb::task_scheduler_init::default_num_threads();
+    std::cout << "Max num threads: " << maxNumThreads << std::endl;
+
+    for (int numThreads = 1; numThreads <= maxNumThreads; numThreads++)
+      {
+      tbb::task_scheduler_init tbbSchedulerInit(numThreads);
+      for (int trial = 0; trial < NUM_TRIALS; trial++)
+        {
+        dax::cont::Scheduler<DeviceAdapter> scheduler;
+
+#ifdef LOAD_DATA
+        ArrayHandleScalar originalArray =
+            dax::cont::make_ArrayHandle(buffer, Container(), DeviceAdapter());
+        ArrayHandleScalar inArray;
+        std::cout << "Copying data for memory affinity." << std::endl;
+        scheduler.Invoke(CopyWorklet(), originalArray, inArray);
+#else // LOAD_DATA
+        std::cout << "Creating data" << std::endl;
+        ArrayHandleScalar inArray;
+        CreateMagnitudeField(grid, inArray);
+#endif // LOAD_DATA
+        assert(grid.GetNumberOfPoints() == inArray.GetNumberOfValues());
+
+//        std::cout << "Computing gradient..." << std::endl;
+//        dax::cont::ArrayHandle<dax::Vector3> gradient;
+//        dax::cont::worklet::CellGradient(
+//              grid, grid.GetPointCoordinates(), inArray, gradient);
+//        inArray.ReleaseResources();
+
+//        std::cout << "Computing magnitude..." << std::endl;
+//        dax::cont::ArrayHandle<dax::Scalar> magnitude;
+//        dax::cont::worklet::Magnitude(gradient, magnitude);
+//        gradient.ReleaseResources();
+
+//        std::cout << "Writing gradient magnitude..." << std::endl;
+//        WriteGradientMagnitude(magnitude);
+//        std::cout << "Data written." << std::endl;
+
+        std::cout << "Computing Threshold, " << numThreads << " threads..."
+                  << std::endl;
+        dax::cont::Timer<DeviceAdapter> timer;
+        typedef dax::cont::ArrayHandle<dax::Id, Container, DeviceAdapter>
+            ClassifyResultType;
+        typedef dax::cont::GenerateTopology<
+            dax::worklet::ThresholdTopology,ClassifyResultType>
+            GenerateTopologyType;
+
+        // Run classify algorithm (determine how many cells are passed).
+#ifdef LOAD_DATA
+        const dax::Scalar LOW_SCALAR = 0.07;
+        const dax::Scalar HIGH_SCALAR = 1.0;
+#else // LOAD_DATA
+        const dax::Scalar LOW_SCALAR = 50.0;
+        const dax::Scalar HIGH_SCALAR = 200.0;
+#endif // LOAD_DATA
+        ClassifyResultType classificationArray;
+        scheduler.Invoke(dax::worklet::ThresholdClassify<dax::Scalar>(
+                           LOW_SCALAR, HIGH_SCALAR),
+                         grid,
+                         inArray,
+                         classificationArray);
+
+        // Build thresholded topology.
+        GenerateTopologyType resolveTopology(classificationArray);
+#ifndef REMOVE_UNUSED_POINTS
+        resolveTopology.SetRemoveDuplicatePoints(false);
+#endif
+        UnstructuredGridType outGrid;
+        scheduler.Invoke(resolveTopology, grid, outGrid);
+
+#ifdef REMOVE_UNUSED_POINTS
+        // Compact scalar array to new topology.
+        ArrayHandleScalar outArray;
+        resolveTopology.CompactPointField(inArray, outArray);
+
+        // Copy grid information to host, if necessary.
+        outGrid.GetCellConnections().GetPortalConstControl();
+        outGrid.GetPointCoordinates().GetPortalConstControl();
+        outArray.GetPortalConstControl();
+#endif
+
+        dax::Scalar time = timer.GetElapsedTime();
+        std::cout << "Number of output cells: " << outGrid.GetNumberOfCells()
+                  << std::endl;
+        std::cout << "Time: " << time << " seconds" << std::endl;
+        fprintf(fd, "Dax-TBB,%d,%d,%lg\n",numThreads,trial,time);
+//        std::cout << "CSV," << description << "," << time << "," << trial << std::endl;
+        }
+      }
+
+//    std::cout << "Writing threshold geometry..." << std::endl;
+//    std::ofstream file;
+//    file.open("/Users/kmorel/Downloads/thresholdGeometry.vtk");
+//    PrintContentsToStream(outGrid, file);
+//    file.close();
+//    std::cout << "Done." << std::endl;
+    }
+    catch (dax::cont::Error error)
+    {
+      std::cout << "Caught Dax error: " << std::endl
+                << error.GetMessage() << std::endl;
+      return 1;
+    }
+
+    fclose(fd);
+
+    return 0;
+  }
+};
+
+int main(int, char *[])
+{
+  return ThresholdExample::Run();
+}
+
